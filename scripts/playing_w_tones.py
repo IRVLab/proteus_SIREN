0fde96052ea7e924c50407b36ce9430967d87ab6
Michael Fulton <fulto081@umn.edu>
2022-08-11 17:39:21 -0500

    Some tone testing

---
+++ scripts/playing_w_tones.py
@@ -0,0 +1,22 @@
+from tones import SINE_WAVE, SAWTOOTH_WAVE, SQUARE_WAVE, TRIANGLE_WAVE
+from tones.mixer import Mixer
+
+from pydub import AudioSegment
+from pydub.playback import play
+
+mixer = Mixer(44100,0.5)
+
+mixer.create_track(0, SAWTOOTH_WAVE, vibrato_frequency=0, vibrato_variance=0, attack=0.001, decay=0.01)
+
+mixer.add_note(0, note="c", octave=4, duration=1.0)
+mixer.add_note(0, note="d", octave=4, duration=1.0)
+mixer.add_note(0, note="e", octave=4, duration=1.0)
+mixer.add_note(0, note="f", octave=4, duration=1.0)
+mixer.add_note(0, note="g", octave=4, duration=1.0)
+mixer.add_note(0, note="a", octave=4, duration=1.0)
+mixer.add_note(0, note="b", octave=4, duration=1.0)
+
+mixer.write_wav("./test.wav")
+
+a = AudioSegment.from_wav("./test.wav")
+play(a)
