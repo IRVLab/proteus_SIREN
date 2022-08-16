1539bd871d7440bfb1e5952c23032d84ae353923
Michael Fulton <fulto081@umn.edu>
2022-08-16 11:16:28 -0500

    Quantity synth implemented

--- scripts/synth_siren_server.py
+++ scripts/synth_siren_server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import rospy
 from rosnode import get_node_names
 from rospy import service
@@ -18,6 +19,7 @@ import xml.etree.ElementTree as ET
 from proteus.srv import SymbolTrigger, SymbolDirectional, SymbolTarget, SymbolQuantity
 from proteus.soneme import Soneme, SNode, SNodeTone
 from proteus.siren import SirenConfig
+from proteus.tone import Tone, RunTone
 
 rospy.init_node('ogg_siren_server', argv=None, anonymous=True)
 siren_config = None
@@ -74,7 +76,6 @@ def execute_trigger(req, soneme):
 
     a = AudioSegment.from_ogg(duck_fn)
     a._data = mixer.sample_data()
-
     play(a)
 
     return True
@@ -87,7 +88,56 @@ def execute_target(req, soneme):
     pass
 
 def execute_quantity(req, soneme):
-    pass
+    mixer = Mixer(44100,0.5)
+    tracks = dict()
+
+    for sn in soneme.snodes:
+        d = sn.duration
+        if len(sn.tones)> 0:
+            for t in sn.tones:
+                track_id = t.track_id
+
+                # Only create the track if it hasn't been created before.
+                if track_id not in tracks.keys():
+                    idx = len(tracks.keys())
+                    tracks[track_id] = idx
+
+                    tdef = siren_config.synth_tracks[track_id]
+
+                    if tdef.wave_type == "sine":
+                        wave_type = SINE_WAVE
+                    elif tdef.wave_type == "saw":
+                        wave_type = SAWTOOTH_WAVE
+                    elif tdef.wave_type == "square":
+                        wave_type = SQUARE_WAVE
+                    elif tdef.wave_type == "triangle":
+                        wave_type = TRIANGLE_WAVE
+
+                    mixer.create_track(idx,wave_type, vibrato_frequency=tdef.vibrato, vibrato_variance=tdef.vibrato_variance, attack=tdef.attack, decay=tdef.decay)
+
+                if type(t) == Tone:
+                    mixer.add_note(tracks[track_id], note=t.note, octave=t.octave, endnote=t.end_note, duration=d.seconds)
+                elif type(t) == RunTone:
+                    tone_values = t.get_dyn_notes(req.quantity, sn.duration.seconds, sn.quantity.min_amount, sn.quantity.max_amount)
+                    # We get note, octave, and duration. If note/octave are None, simply add silence.
+                    for n, o, d in tone_values:
+                        if n:
+                            mixer.add_note(tracks[track_id], note=n, octave=o, duration=d)
+                        else:
+                            mixer.add_silence(tracks[track_id], duration=d)
+
+        else:
+            mixer.add_silence(0, duration=d.seconds)
+
+    # We need an audio segment, so load in the duck, then replace the data with our mixer's data.
+    dir = siren_config.clip_location
+    duck_fn = dir + '/duck_test.ogg'
+
+    a = AudioSegment.from_ogg(duck_fn)
+    a._data = mixer.sample_data()
+    play(a)
+
+    return True
 
 if __name__ == '__main__':
     rospy.loginfo('Initializing the SIREN server')
